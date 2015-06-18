af7ad28e7c595945a661f91fc406e10c9c876350
gnoumy <gaucher.ju@gmail.com>
2015-06-18 22:50:53 +0200

    ajout d'un formulaire de test

--- src/blogBundle/Controller/AdminController.php
+++ src/blogBundle/Controller/AdminController.php
@@ -2,12 +2,19 @@
 
 namespace blogBundle\Controller;
 
+use blogBundle\Form\CategoriesType;
+use blogBundle\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('blogBundle:Admin:index.html.twig');
+
+    	$categories = new Categories();
+    	$form = $this->createForm(new CategoriesType(), $categories);
+        return $this->render('blogBundle:Admin:index.html.twig', array(
+        	'form' => $form->createView())
+        );
     }
 }
