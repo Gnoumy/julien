f9acefbb9223b61a0d67b3e06b3c1434e016dbaf
gnoumy <gaucher.ju@gmail.com>
2015-06-20 03:45:37 +0200

    modification de la sécurité et des routes pagePhoto + pageCategorie

--- src/blogBundle/Entity/Utilisateurs.php
+++ src/blogBundle/Entity/Utilisateurs.php
@@ -25,22 +25,7 @@ class Utilisateurs extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="pseudo", type="string", length=255)
-     */
-    protected $pseudo;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="mdp", type="string", length=255)
-     */
-    protected $mdp;
-
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="role", type="string", length=255)
+     * @ORM\Column(name="role", type="string", length=255, nullable=True)
      */
     protected $role;
 
@@ -59,52 +44,6 @@ class Utilisateurs extends BaseUser
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set pseudo
-     *
-     * @param string $pseudo
-     * @return Utilisateurs
-     */
-    public function setPseudo($pseudo)
-    {
-        $this->pseudo = $pseudo;
-
-        return $this;
-    }
-
-    /**
-     * Get pseudo
-     *
-     * @return string 
-     */
-    public function getPseudo()
-    {
-        return $this->pseudo;
-    }
-
-    /**
-     * Set mdp
-     *
-     * @param string $mdp
-     * @return Utilisateurs
-     */
-    public function setMdp($mdp)
-    {
-        $this->mdp = $mdp;
-
-        return $this;
-    }
-
-    /**
-     * Get mdp
-     *
-     * @return string 
-     */
-    public function getMdp()
-    {
-        return $this->mdp;
     }
 
     /**
